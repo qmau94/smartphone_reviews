88a33f00ae22e02d3c419bf4f874394f6cb841f7
qmau94 <qmau94@gmail.com>
2016-11-23 15:40:41 +0700

    add-rate-to-review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,10 @@ class Review < ApplicationRecord
   belongs_to :mobile, optional: true
   has_many :comments, as: :commentable, dependent: :destroy
 
+  ratyrate_rateable "original_score"
+
+  scope :with_ratings, ->{includes(:rate_average_without_dimension).order("rating_caches.avg desc")}
+
   has_attached_file :image, styles: { medium: "730x230>", thumb: "350x150>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
--- db/migrate/20161116163923_add_parent_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddParentToComments < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :comments, :parent, foreign_key: true
-  end
-end
